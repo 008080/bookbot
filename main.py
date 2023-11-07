2b12b62af835a235b7eb2c89e72c22874c6f757b
008080 <info@vassi.at>
2023-11-08 00:23:16 +0100

    finished learning project

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-print("hi")
+with open("books/frankenstein.txt") as f:
+    file_content = f.read()
+
+def word_count(text):
+    words = len(text.split())
+    return words
+
+def letter_count(text):
+    letters = {}
+    for i in text.lower():
+        if i.isalpha():
+            if i not in letters:
+                letters[i] = 1
+            else:
+                letters[i]+= 1
+    list = []
+    for i in letters:
+        list.append([letters[i], i])
+    list.sort(reverse=True, )
+    print_letter_count(list)
+
+def print_letter_count(list):
+    for i in list:
+        print(f" The '{i[1]}' character was found {i[0]} times")
+
+print("--- Begin report of books/frankenstein.txt ---")        
+print(f"{word_count(file_content)} words found in the document")
+print("\n\n")
+letter_count(file_content)
+print("--- End report ---")
